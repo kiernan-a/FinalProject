b7ae996efb8d949a042b62123c650b4d61e7ee19
Your Name <kda607@gmail.com>
2022-04-02 09:20:41 -0400

    Initial Commit

---
+++ MailApp/MailAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailAppApp.swift
+//  MailApp
+//
+//  Created by Kiernan Almand on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct MailAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
